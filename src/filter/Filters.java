ed489f95b1efb9d052e925154064267a93526ad9
Francisco Flores <pancho.lbt@gmail.com>
2024-04-29 19:43:39 -0300

    blur function implemented

--- src/filter/Filters.java
+++ src/filter/Filters.java
@@ -4,6 +4,51 @@ public class Filters {
     
     public static void blur(int height, int width, RgbTriple[][] image) {
         
+        RgbTriple[][] tempImage = new RgbTriple[height][width];
+        
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                
+                int totalBlue, totalGreen, totalRed;
+                totalBlue = totalGreen = totalRed = 0;
+                float counter = 0f;
+                
+                for (int y = row - 1; y <= row + 1; y++) {
+                    for (int x = column - 1; x <= column + 1; x++) {
+                        
+                        if (y > -1 && y < height && x > -1 && x < width) {
+                            //System.out.println("Entro");
+                            totalBlue += Byte.toUnsignedInt(image[y][x].blue);
+                            totalGreen += Byte.toUnsignedInt(image[y][x].green);
+                            totalRed += Byte.toUnsignedInt(image[y][x].red);
+                            counter++;
+                            //if ((row % 100) == 0)
+                              //  System.out.println("blue: " + totalBlue + "  green: " + totalGreen + "  red: " + totalRed);
+                        }
+                        
+                    }
+                }
+                byte blue = (byte) Math.round(totalBlue / counter);
+                byte green = (byte) Math.round(totalGreen / counter);
+                byte red = (byte) Math.round(totalRed / counter);
+                
+                /*
+                int blueint = Byte.toUnsignedInt(blue);
+                int greenint = Byte.toUnsignedInt(green);
+                int redint = Byte.toUnsignedInt(red);
+                
+                if ((column % 100) == 0)
+                    System.out.println("blue: " + blueint + "  green: " + greenint + "  red: " + redint);*/
+                
+                tempImage[row][column] = new RgbTriple(blue, green, red);
+                
+            } 
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                image[i][j] = tempImage[i][j];
+            }  
+        }
     }
     
     public static void edge(int height, int width, RgbTriple[][] imagee) {
